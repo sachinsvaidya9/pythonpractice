1fcd037b2648b7faffa4f2c8a4bcb4ace75a5985
sachinsvaidya <sachinsvaidya9@gmail.com>
2023-05-28 18:41:59 +0530

    string char and alpha count

---
+++ stringPrg.py
@@ -0,0 +1,16 @@
+strg = input("Pls enter a string: ")
+char = input("Give character to count from strg: ")
+
+noChar = len(strg)
+
+charCount = strg.count(char)
+
+print("Number of characters in the given string: ", noChar)
+
+print("Number of occurences of the given character in the string: ", charCount)
+
+
+
+
+
+
